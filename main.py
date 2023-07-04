bb976c02cc0aa6b273781551ee87d65b5b09f866
Sav <74868245+IT-Sav@users.noreply.github.com>
2023-07-04 17:04:08 +0100

    Add files via upload
    More improvements

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 import json
 import random
 from datetime import datetime
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 
 app = Flask(__name__)
+app.secret_key = "your_secret_key"
 
 # Function to load user data
 def load_user():
     with open("info.json", "r") as file:
-        user = json.load(file)
-        return user
+        users = json.load(file)
+        return users if isinstance(users, list) else [users]
+
+# Function to find a user by name and password
+def find_user(name, password):
+    users = load_user()
+    for user in users:
+        if user["name"] == name and user["password"] == password:
+            return user
+    return None
 
 # Function to perform payment
 def perform_payment(name, creditid):
@@ -84,54 +93,79 @@ def login():
                 "creditid": creditid
             }
 
-            with open("info.json", "a") as file:
-                json.dump(user, file)
+            users = load_user()
+            users.append(user)
+
+            with open("info.json", "w") as file:
+                json.dump(users, file)
+
+            session["name"] = user["name"]
+            session["password"] = user["password"]
 
         else:
-            user = load_user()
+            name = request.form['name']
+            password = request.form['password']
 
-            while True:
-                name = request.form['name']
-                dob = request.form['dob']
-                password = request.form['password']
+            user = find_user(name, password)
 
-                if name == user["name"] and dob == user["dob"] and password == user["password"]:
-                    break
-                else:
-                    return render_template('login.html', message="Sorry, this information is incorrect. Please try again.")
+            if user is None:
+                return render_template('login.html', message="Sorry, this information is incorrect. Please try again.")
 
+            session["name"] = user["name"]
+            session["password"] = user["password"]
+
+        session["authenticated"] = True
         return redirect(url_for('dashboard'))  # Redirect to the dashboard page
 
     return render_template('login.html')
 
 @app.route('/dashboard')
 def dashboard():
-    user = load_user()
+    if "authenticated" not in session or not session["authenticated"]:
+        return redirect(url_for('login'))
+
+    user = find_user(session["name"], session["password"])
+
     return render_template('dashboard.html', user=user)
 
 @app.route('/payment', methods=['POST'])
 def payment():
-    user = load_user()
+    if "authenticated" not in session or not session["authenticated"]:
+        return redirect(url_for('login'))
+
+    user = find_user(session["name"], session["password"])
+
     return perform_payment(user["name"], user["creditid"])
 
 @app.route('/transactions')
 def transactions():
+    if "authenticated" not in session or not session["authenticated"]:
+        return redirect(url_for('login'))
+
     # Implement recent transactions functionality
     return render_template('transactions.html')
 
 @app.route('/balance')
 def balance():
+    if "authenticated" not in session or not session["authenticated"]:
+        return redirect(url_for('login'))
+
     # Implement account balance functionality
     return render_template('balance.html')
 
 @app.route('/settings')
 def settings():
+    if "authenticated" not in session or not session["authenticated"]:
+        return redirect(url_for('login'))
+
     # Implement account settings functionality
     return render_template('settings.html')
 
 @app.route('/logout')
 def logout():
-    # Code for logging out
+    session.pop("authenticated", None)
+    session.pop("name", None)
+    session.pop("password", None)
     return redirect(url_for('home'))  # Redirect back to the homepage
 
 # Run the Flask application
